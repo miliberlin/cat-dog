f0ad7b47ee011c58f6e605fdc96925e8b81bd7e9
miliberlin <michelle.liebheit@posteo.de>
2019-10-31 12:08:41 +0900

    responsive animation

--- animation.js
+++ animation.js
@@ -1,15 +1,39 @@
+//deleting cats
+
+const cat = document.getElementsByClassName("cat");
+
+for (let i = 0; i < cat.length; i++) {
+  cat[i].addEventListener('click', function() {
+    this.parentNode.removeChild(this);
+    if (cat.length === 0) {
+      whenCatEmpty()
+    }
+  });
+}
+
+function whenCatEmpty() {
+  // congrats text
+  
+  // add video
+const iframe = document.createElement("iframe");
+iframe.src = "https://www.youtube.com/embed/SB-qEYVdvXA?autoplay=1";
+iframe.width = "560";
+iframe.height = "315";
+iframe.setAttribute("allow", "autoplay");
+document.getElementById("inner").appendChild(iframe);
+}
+
 // text animation
 // credits: https://tobiasahlin.com/moving-letters/#3
 
 var textWrapper = document.querySelector('.ml3');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: false})
   .add({
-    targets: '.ml3 .letter',
+    targets: '.ml3',
     opacity: [0,1],
     easing: "easeInOutQuad",
-    duration: 1000,
+    duration: 3000,
     delay: (el, i) => 100 * (i+1)
   }).add({
     targets: '.ml3',
@@ -19,14 +43,4 @@ anime.timeline({loop: false})
     delay: 1000
   });
 
-//deleting cats
-
-const cat = document.querySelectorAll(".cat");
-
-function remove() {
-  this.parentNode.removeChild(this);
-}
-
-for (i = 0; i < cat.length; i++) {
-  cat[i].addEventListener('click', remove, false);
-}
+  
