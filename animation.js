804f1c1cb2c62570a6a6e9e59de984d49ac17d57
miliberlin <michelle.liebheit@posteo.de>
2019-10-31 13:57:37 +0900

    added dogs

--- animation.js
+++ animation.js
@@ -1,26 +1,49 @@
-//deleting cats
+// item = dog/cat
 
-const cat = document.getElementsByClassName("cat");
+// deleting items
 
-for (let i = 0; i < cat.length; i++) {
-  cat[i].addEventListener('click', function() {
+const item = document.getElementsByClassName("item");
+
+for (let i = 0; i < item.length; i++) {
+  item[i].addEventListener('click', function() {
     this.parentNode.removeChild(this);
-    if (cat.length === 0) {
-      whenCatEmpty()
+    if (item.length === 0) {
+      whenItemEmpty()
     }
   });
 }
 
-function whenCatEmpty() {
+function whenItemEmpty() {
   // congrats text
-  
+  setTimeout(function(){ 
+    const congratsText = document.createElement("h1");
+    congratsText.setAttribute("class", "ml3");
+    congratsText.innerHTML = ("Awesome!<br>You did it!");
+    document.getElementById("inner").appendChild(congratsText);
+    }, 500);
+
   // add video
-const iframe = document.createElement("iframe");
-iframe.src = "https://www.youtube.com/embed/SB-qEYVdvXA?autoplay=1";
-iframe.width = "560";
-iframe.height = "315";
-iframe.setAttribute("allow", "autoplay");
-document.getElementById("inner").appendChild(iframe);
+  setTimeout(function(){
+    const iframe = document.createElement("iframe");
+    if (window.location.href==="file:///Users/Michelle/Desktop/7projects-7days/cat-dog/cat.html") {
+      iframe.src = "https://www.youtube.com/embed/SB-qEYVdvXA?autoplay=1";
+    }
+    else {
+      iframe.src = "https://www.youtube.com/embed/mRf3-JkwqfU?start=8?autoplay=1";
+    }
+    iframe.width = "560";
+    iframe.height = "315";
+    iframe.setAttribute("allow", "autoplay");
+    document.getElementById("inner").appendChild(iframe);
+    // add return button
+    const returnBtn = document.createElement("a");
+    returnBtn.setAttribute("href", "index.html");
+    returnBtn.setAttribute("id", "return-btn");
+    returnBtn.setAttribute("class", "btn");
+    const returnBtnText = document.createElement("div");
+    returnBtnText.innerHTML = ("Play again");    document.getElementById("inner").appendChild(returnBtn);
+    document.getElementById("return-btn").appendChild(returnBtnText);
+  }, 4000);
 }
 
 // text animation
@@ -33,7 +56,7 @@ anime.timeline({loop: false})
     targets: '.ml3',
     opacity: [0,1],
     easing: "easeInOutQuad",
-    duration: 3000,
+    duration: 2500,
     delay: (el, i) => 100 * (i+1)
   }).add({
     targets: '.ml3',
